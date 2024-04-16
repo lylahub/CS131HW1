f1bf19b9d3fa80585e8385bee34e561d29234b9a
Lyla Liu <lylaliu@Lylas-MBP.lan>
2024-04-15 19:40:50 -0700

    add types to each func

--- hw1.ml
+++ hw1.ml
@@ -1,5 +1,8 @@
 (* 1. The function subset a b returns true iff a ⊆ b *)
-
+(* input:  a:'a list
+            b:'a list
+    output: bool
+   type: val subset : 'a list -> 'a list -> bool = <fun>  *)
 let rec subset a b =
   match a with 
   | [] -> true
@@ -7,11 +10,18 @@ let rec subset a b =
 
 
 (* 2. The function equal_sets a b returns true iff the represented sets are equal. *)
+(* input:  a:'a list 
+            b:'a list
+    output: bool
+    val equal_sets : 'a list -> 'a list -> bool = <fun> *)
 let equal_sets a b = 
   subset a b && subset b a
 
 
 (* 3. The function set_union a b returns a list representing a U b. *)
+(* input:  a:'a list ; b:'a list
+    output: 'a list
+    val set_union : 'a list -> 'a list -> 'a list = <fun> *)
 let rec set_union a b = 
   match a with
   | [] -> b
@@ -21,6 +31,9 @@ let rec set_union a b =
 (* 4. The function set_all_union a returns a list representing ∪[x ∈ a]x 
 Go through all elements and apply set_union to all element, use the built 
 in function fold_left *)
+(* input:  a:'a list list
+    output: 'a list
+    val set_all_union : 'a list list -> 'a list = <fun> *)
 let set_all_union a = 
   List.fold_left set_union [] a
 
@@ -37,6 +50,11 @@ impossible to do in OCaml due to its type check *)
 
 (* 6. The function computed_fixed_point eq f x returns the computed fixed point for 
 f with respect to x, assuming that eq is the equality predicate for f's domain.*)
+(* input:  eq: 'a -> 'a -> bool = <fun>
+            f: 'a -> 'a = <fun> 
+            x: 'a 
+    output: 'a 
+    val computed_fixed_point : ('a -> 'a -> bool) -> ('a -> 'a) -> 'a -> 'a = <fun> *)
 let rec computed_fixed_point eq f x = 
   if eq x (f x) then x
   else computed_fixed_point eq f (f x)
@@ -45,6 +63,12 @@ let rec computed_fixed_point eq f x =
 (* 7. The function computed_periodic_point eq f p x returns the computed periodic 
 point for f with period p and with respect to x, assuming that eq is the equality 
 predicate for f's domain. *)
+(* input:  eq: 'a -> 'a -> bool = <fun> 
+            f: 'a -> 'a = <fun>
+            p: int
+            x: 'a 
+    output: 'a 
+    val computed_periodic_point : ('a -> 'a -> bool) -> ('a -> 'a) -> int -> 'a -> 'a = <fun> *)
 let rec computed_periodic_point eq f p x = 
   let rec apply f p x = 
     if p > 0 then apply f (p-1) (f x)
@@ -56,6 +80,11 @@ let rec computed_periodic_point eq f p x =
 
 (* 8. The function whileseq s p x returns the longest list [x; s x; s (s x); ...] 
 such that p e is true for every element e in the list. *)
+(* input:  s: 'a -> 'a = <fun> 
+            p: 'a -> bool = <fun>
+            x: 'a 
+    output: 'b list 
+    val whileseq : ('a -> 'a) -> ('a -> bool) -> 'a -> 'a list = <fun> *)
 let rec whileseq s p x = 
   if (p x) then (x::(whileseq s p (s x)))
   else []
@@ -63,23 +92,37 @@ let rec whileseq s p x =
 
 (* 9. The function filter_blind_alleys g returns a copy of the grammar g with 
 all blind-alley rules removed. This function preserve the order of rules. *)
+
+(* define type for symbol *)
 type ('nonterminal, 'terminal) symbol =
   | N of 'nonterminal
   | T of 'terminal
 
 (* aux function checking if an element is contained in a list*)
+(* input:  elem:'a
+            lst:'a list
+    output: bool
+    type: val is_in : 'a -> 'a list -> bool = <fun> *)
 let rec contain_element e l = 
   match l with 
   |[] -> false
   | h :: t -> (e = h) || (contain_element e t)
 
-(* aux func checking whether a symbol is terminable*)
+(* aux func checking whether a symbol is terminable *)
+(* input:  symbol: ('a, 'b) symbol 
+            terminable_symbol_list: ('a, 'b) symbol list
+    output: bool
+    val is_symbol_terminable : ('a, 'b) symbol  -> ('a, 'b) symbol list -> bool = <fun> *)
 let symbol_terminability s l =
   match s with
   | T terminal -> true
   | N nonterminal -> contain_element s l
 
 (* aux func check rhs only contains terminable symbols*)
+(* input:  rhs: ('a, 'b) symbol list
+            terminable_symbol_list: ('a, 'b) symbol list
+    output: bool
+    val is_rhs_all_terminable : ('a, 'b) symbol list -> ('a, 'b) symbol list -> bool = <fun> *)
 let rec rhs_terminability rhs l =
   match rhs with
   | [] -> true
@@ -87,6 +130,10 @@ let rec rhs_terminability rhs l =
               else false
 
 (* aux func return ('a, 'b) symbol list with all terminable symbols in rules *)
+(* input:  rules: (('a, 'b) symbol * ('a, 'b) symbol list) list
+            terminable_symbol_list: ('a, 'b) symbol list
+    output: ('a, 'b) symbol list
+    val construct_terminable_symbol_list : ('a * ('a, 'b) symbol list) list -> ('a, 'b) symbol list -> ('a, 'b) symbol list = <fun> *)
 let rec terminable_symbol_list rules l =
   match rules with
   | [] -> l
@@ -96,19 +143,33 @@ let rec terminable_symbol_list rules l =
 
 
 (* aux func returns a tuple with rules, list containing terminalb symbols*)
+(* input:  (rules, terminable_symbol_list): ((('a, 'b) symbol * ('a, 'b) symbol list) list * ('a, 'b) symbol list)
+   output: (rules, terminable_symbol_list): ((('a, 'b) symbol * ('a, 'b) symbol list) list * ('a, 'b) symbol list)
+   val rules_with_terminable_symbol_list : ('a * ('a, 'b) symbol list) list * ('a, 'b) symbol list -> ('a * ('a, 'b) symbol list) list * ('a, 'b) symbol list = <fun> *)
 let rules_symbols (rules, l) =
   (rules, terminable_symbol_list rules l)
 
 (* aux func check whether two results from the prev func are the same*)
+(* input:  (rules1, terminable_symbol_list1): ((('a, 'b) symbol * ('a, 'b) symbol list) list * ('a, 'b) symbol list)
+            (rules2, terminable_symbol_list2): ((('a, 'b) symbol * ('a, 'b) symbol list) list * ('a, 'b) symbol list)
+    output: bool
+    val equal_terminable_symbol_list : 'a * 'b list -> 'c * 'b list -> bool = <fun> *)
 let equal_rules_symbols (r1, l1) (r2, l2) =
   equal_sets l1 l2
 
 (* aux func to generate tuple with rules and terminable symbols *)
+(* input:  rules: (('a, 'b) symbol * ('a, 'b) symbol list) list
+            terminable_symbol_list: ('a, 'b) symbol list
+    output: ((('a, 'b) symbol * ('a, 'b) symbol list) list * ('a, 'b) symbol list)
+    val final_terminable_symbol_list : ('a * ('a, 'b) symbol list) list -> ('a, 'b) symbol list -> ('a * ('a, 'b) symbol list) list * ('a, 'b) symbol list = <fun> *)
 let final_list r l =
   computed_fixed_point (equal_rules_symbols) (rules_symbols) (r, l)
 
 (* aux func check rhs only contain terminable symbol for all rules
 remove rules with unterminable rhs*)
+(* input:  (rules, terminable_symbol_list): ((('a, 'b) symbol * ('a, 'b) symbol list) list * ('a, 'b) symbol list)
+    output: (('a, 'b) symbol * ('a, 'b) symbol list) list 
+    val remove_bad_rules : ('a * ('b, 'c) symbol list) list * ('b, 'c) symbol list -> ('a * ('b, 'c) symbol list) list = <fun> *)
 let rec remove_rules (r, l) = 
   match r with
   | (s, rhs) :: t -> if (rhs_terminability rhs l) 
@@ -117,11 +178,10 @@ let rec remove_rules (r, l) =
   | [] -> []
 
 (* final function *)
+(* input:  g: ('a, 'b) symbol * (('a, 'b) symbol * ('a, 'b) symbol list) list
+    output: ('a, 'b) symbol * (('a, 'b) symbol * ('a, 'b) symbol list) list
+    val filter_blind_alleys : 'a * ('b * ('b, 'c) symbol list) list -> 'a * ('b * ('b, 'c) symbol list) list = <fun> *)
 let filter_blind_alleys g =
   match g with
   | (start, rules) -> (start, (remove_rules (final_list rules [])))
-
-(* 10. Supply at least one test case for each of the above functions in the style shown in the sample test cases below. 
-When testing the function F, call the test cases my_F_test0, my_F_test1, etc. For example, for subset your first test case 
-should be called my_subset_test0. Your test cases should exercise all the above functions, even though the sample test cases do not.*)
 
