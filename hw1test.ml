2dc667cbeab98c68b43c7c9399526106019a5cd3
Lyla Liu <lylaliu@Lylas-MBP.lan>
2024-04-15 20:23:36 -0700

    update test cases

---
+++ hw1test.ml
@@ -0,0 +1,186 @@
+(* Q1 *)
+let subset_test0 = subset [] []
+let subset_test1 = subset [] [1;4;2;3]
+let subset_test2 = not (subset [1;5;7] [4;3])
+
+(* Q2 *)
+let equal_sets_test0 = not (equal_sets [1; 2] [1; 2; 3])
+let equal_sets_test1 = equal_sets [] []
+let equal_sets_test2 = equal_sets [-1; -3; -5] [-5; -1; -3]
+
+(* Q3 *)
+let my_set_union_test0 = equal_sets (set_union [] []) []
+let my_set_union_test1 = equal_sets (set_union [1;1;2] [2;3;3]) [1;2;3]
+let my_set_union_test2 = equal_sets (set_union [1;3;5] [2;4;5]) [1;2;3;4;5]
+
+(* Q4 *)
+let my_set_all_union_test0 = equal_sets (set_all_union []) []
+let my_set_all_union_test1 = equal_sets (set_all_union [[7;8]; [8;9]; [10]]) [7;8;9;10]
+let my_set_all_union_test2 = equal_sets (set_all_union [[1;2]; [1;2]; [1;2]]) [1;2]
+
+(* Q6 *)
+let my_computed_fixed_point_test0 = computed_fixed_point (=) (fun x -> x *. 0.5) 100. = 0.
+let my_computed_fixed_point_test1 = computed_fixed_point (=) log 1000000. = 0.
+let my_computed_fixed_point_test2 = computed_fixed_point (=) (fun x -> x) 999. = 999.
+
+(* Q7 *)
+let my_computed_periodic_point_test0 = computed_periodic_point (=) (fun _ -> 5) 3 10 = 5
+let my_computed_periodic_point_test1 = computed_periodic_point (=) (fun x -> x + 1) 1 10 = 10
+
+(* Q8 *)
+let my_whileseq_test0 = equal_sets (whileseq (fun x -> x - 1) (( > ) 0) 5) [5; 4; 3; 2; 1]
+let my_whileseq_test1 = equal_sets (whileseq (fun x -> x * x) (( < ) 10000) 2) [2; 4; 16; 256]
+
+(* Q9 *)
+type awksub_nonterminals =
+  | Expr | Lvalue | Incrop | Binop | Num
+
+let awksub_rules_single_recursive = [Expr, [N Expr]]
+let awksub_grammar_single_recursive = Expr, awksub_rules_single_recursive
+let my_filter_blind_alleys_test8 = filter_blind_alleys awksub_grammar_single_recursive = (Expr, [])
+
+let awksub_rules_mixed_productive = [Expr, [T "("; N Expr; T ")"]; Expr, [N Num]; Num, [T "1"]]
+let awksub_grammar_mixed_productive = Expr, awksub_rules_mixed_productive
+let my_filter_blind_alleys_test9 = filter_blind_alleys awksub_grammar_mixed_productive = (Expr, [Expr, [N Num]; Num, [T "1"]])
+
+let awksub_rules_interdependent = [Expr, [N Binop; N Num]; Binop, [N Expr]; Num, [T "0"]]
+let awksub_grammar_interdependent = Expr, awksub_rules_interdependent
+let my_filter_blind_alleys_test10 = filter_blind_alleys awksub_grammar_interdependent = (Expr, [Expr, [N Binop; N Num]; Num, [T "0"]])
+
+let awksub_rules_deeply_nested = [Expr, [N Lvalue]; Lvalue, [N Expr]; Expr, [T "0"]]
+let awksub_grammar_deeply_nested = Expr, awksub_rules_deeply_nested
+let my_filter_blind_alleys_test11 = filter_blind_alleys awksub_grammar_deeply_nested = (Expr, [Expr, [T "0"]])
+
+let awksub_rules_all_leading_to_terminal = [Expr, [T "("; N Expr; T ")"]; Expr, [N Num]; Num, [T "9"]]
+let awksub_grammar_all_leading_to_terminal = Expr, awksub_rules_all_leading_to_terminal
+let my_filter_blind_alleys_test12 = filter_blind_alleys awksub_grammar_all_leading_to_terminal = awksub_grammar_all_leading_to_terminal
+
+
+(* Provided Testcases *)
+(* let subset_test0 = subset [] [1;2;3]
+let subset_test1 = subset [3;1;3] [1;2;3]
+let subset_test2 = not (subset [1;3;7] [4;1;3])
+
+let equal_sets_test0 = equal_sets [1;3] [3;1;3]
+let equal_sets_test1 = not (equal_sets [1;3;4] [3;1;3])
+
+let set_union_test0 = equal_sets (set_union [] [1;2;3]) [1;2;3]
+let set_union_test1 = equal_sets (set_union [3;1;3] [1;2;3]) [1;2;3]
+let set_union_test2 = equal_sets (set_union [] []) []
+
+let set_all_union_test0 =
+  equal_sets (set_all_union []) []
+let set_all_union_test1 =
+  equal_sets (set_all_union [[3;1;3]; [4]; [1;2;3]]) [1;2;3;4]
+let set_all_union_test2 =
+  equal_sets (set_all_union [[5;2]; []; [5;2]; [3;5;7]]) [2;3;5;7]
+
+let computed_fixed_point_test0 =
+  computed_fixed_point (=) (fun x -> x / 2) 1000000000 = 0
+let computed_fixed_point_test1 =
+  computed_fixed_point (=) (fun x -> x *. 2.) 1. = infinity
+let computed_fixed_point_test2 =
+  computed_fixed_point (=) sqrt 10. = 1.
+let computed_fixed_point_test3 =
+  ((computed_fixed_point (fun x y -> abs_float (x -. y) < 1.)
+        (fun x -> x /. 2.)
+        10.)
+    = 1.25)
+
+let computed_periodic_point_test0 =
+  computed_periodic_point (=) (fun x -> x / 2) 0 (-1) = -1
+let computed_periodic_point_test1 =
+  computed_periodic_point (=) (fun x -> x *. x -. 1.) 2 0.5 = -1.
+
+(* An example grammar for a small subset of Awk.  *)
+
+type awksub_nonterminals =
+  | Expr | Lvalue | Incrop | Binop | Num
+
+let awksub_rules =
+    [Expr, [T"("; N Expr; T")"];
+    Expr, [N Num];
+    Expr, [N Expr; N Binop; N Expr];
+    Expr, [N Lvalue];
+    Expr, [N Incrop; N Lvalue];
+    Expr, [N Lvalue; N Incrop];
+    Lvalue, [T"$"; N Expr];
+    Incrop, [T"++"];
+    Incrop, [T"--"];
+    Binop, [T"+"];
+    Binop, [T"-"];
+    Num, [T"0"];
+    Num, [T"1"];
+    Num, [T"2"];
+    Num, [T"3"];
+    Num, [T"4"];
+    Num, [T"5"];
+    Num, [T"6"];
+    Num, [T"7"];
+    Num, [T"8"];
+    Num, [T"9"]]
+
+let awksub_grammar = Expr, awksub_rules
+
+let awksub_test0 =
+  filter_blind_alleys awksub_grammar = awksub_grammar
+
+let awksub_test1 =
+  filter_blind_alleys (Expr, List.tl awksub_rules) = (Expr, List.tl awksub_rules)
+
+let awksub_test2 =
+  filter_blind_alleys (Expr,
+      [Expr, [N Num];
+        Expr, [N Lvalue];
+        Expr, [N Expr; N Lvalue];
+        Expr, [N Lvalue; N Expr];
+        Expr, [N Expr; N Binop; N Expr];
+        Lvalue, [N Lvalue; N Expr];
+        Lvalue, [N Expr; N Lvalue];
+        Lvalue, [N Incrop; N Lvalue];
+        Lvalue, [N Lvalue; N Incrop];
+        Incrop, [T"++"]; Incrop, [T"--"];
+        Binop, [T"+"]; Binop, [T"-"];
+        Num, [T"0"]; Num, [T"1"]; Num, [T"2"]; Num, [T"3"]; Num, [T"4"];
+        Num, [T"5"]; Num, [T"6"]; Num, [T"7"]; Num, [T"8"]; Num, [T"9"]])
+  = (Expr,
+      [Expr, [N Num];
+      Expr, [N Expr; N Binop; N Expr];
+      Incrop, [T"++"]; Incrop, [T"--"];
+      Binop, [T "+"]; Binop, [T "-"];
+      Num, [T "0"]; Num, [T "1"]; Num, [T "2"]; Num, [T "3"]; Num, [T "4"];
+      Num, [T "5"]; Num, [T "6"]; Num, [T "7"]; Num, [T "8"]; Num, [T "9"]])
+
+let awksub_test3 =
+  filter_blind_alleys (Expr, List.tl (List.tl (List.tl awksub_rules))) =
+    filter_blind_alleys (Expr, List.tl (List.tl awksub_rules))
+
+type giant_nonterminals =
+  | Conversation | Sentence | Grunt | Snore | Shout | Quiet
+
+let giant_grammar =
+  Conversation,
+  [Snore, [T"ZZZ"];
+    Quiet, [];
+    Grunt, [T"khrgh"];
+    Shout, [T"aooogah!"];
+    Sentence, [N Quiet];
+    Sentence, [N Grunt];
+    Sentence, [N Shout];
+    Conversation, [N Snore];
+    Conversation, [N Sentence; T","; N Conversation]]
+
+let giant_test0 =
+  filter_blind_alleys giant_grammar = giant_grammar
+
+let giant_test1 =
+  filter_blind_alleys (Sentence, List.tl (snd giant_grammar)) =
+    (Sentence,
+      [Quiet, []; Grunt, [T "khrgh"]; Shout, [T "aooogah!"];
+      Sentence, [N Quiet]; Sentence, [N Grunt]; Sentence, [N Shout]])
+
+let giant_test2 =
+  filter_blind_alleys (Sentence, List.tl (List.tl (snd giant_grammar))) =
+    (Sentence,
+      [Grunt, [T "khrgh"]; Shout, [T "aooogah!"];
+      Sentence, [N Grunt]; Sentence, [N Shout]]) *)
